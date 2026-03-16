ac3c28f52eb826a86291677bf44f7dc5892d7804
Tsutomu-miku <tsutomu.miku@gmail.com>
2026-03-16 20:46:02 +0800

    feat: remove mainland china region check for mobile and tv

--- app/tv/src/main/kotlin/dev/aaa1115910/bv/tv/activities/MainActivity.kt
+++ app/tv/src/main/kotlin/dev/aaa1115910/bv/tv/activities/MainActivity.kt
@@ -4,22 +4,16 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import dev.aaa1115910.bv.repository.UserRepository
 import dev.aaa1115910.bv.tv.screens.MainScreen
-import dev.aaa1115910.bv.tv.screens.RegionBlockScreen
 import dev.aaa1115910.bv.tv.screens.user.lock.UnlockUserScreen
 import dev.aaa1115910.bv.ui.theme.BVTheme
-import dev.aaa1115910.bv.util.NetworkUtil
 import io.github.oshai.kotlinlogging.KotlinLogging
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
 import org.koin.android.ext.android.inject
 
 class MainActivity : ComponentActivity() {
@@ -35,13 +29,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
 
         setContent {
-            val scope = rememberCoroutineScope()
-            var isCheckingNetwork by remember { mutableStateOf(true) }
             var isCheckingUserLock by remember { mutableStateOf(true) }
-            val isChecking by remember {
-                derivedStateOf { isCheckingNetwork || isCheckingUserLock }
-            }
-            var isMainlandChina by remember { mutableStateOf(false) }
             var userLockLocked by remember { mutableStateOf(false) }
 
             LaunchedEffect(Unit) {
@@ -49,36 +37,23 @@ class MainActivity : ComponentActivity() {
                 userLockLocked = user?.lock?.isNotBlank() ?: false
                 logger.info { "default user: ${user?.username}" }
                 isCheckingUserLock = false
-            }
-
-            LaunchedEffect(Unit) {
-                scope.launch(Dispatchers.Default) {
-                    isMainlandChina = NetworkUtil.isMainlandChina()
-                    isCheckingNetwork = false
-                    keepSplashScreen = false
-                }
+                keepSplashScreen = false
             }
 
             BVTheme {
-                if (isChecking) {
-                    //避免在检查网络的期间加载屏幕内容，导致检查完毕后显示屏幕内容时出现初始焦点未成功设置的问题
-                } else if (isMainlandChina) {
-                    RegionBlockScreen()
+                if (isCheckingUserLock) {
+                    // wait for user lock check
+                } else if (!userLockLocked) {
+                    MainScreen()
                 } else {
-                    //HomeScreen()
-                    if (!userLockLocked) {
-                        MainScreen()
-                    } else {
-                        UnlockUserScreen(
-                            onUnlockSuccess = { user ->
-                                logger.info { "unlock user lock for user ${user.uid}" }
-                                userLockLocked = false
-                            }
-                        )
-                    }
+                    UnlockUserScreen(
+                        onUnlockSuccess = { user ->
+                            logger.info { "unlock user lock for user ${user.uid}" }
+                            userLockLocked = false
+                        }
+                    )
                 }
             }
         }
     }
 }
-
